ecea3b1e43e5b5554a05fd385f2981dc83aa5291
Bieefilled <65086865+PhetSirmhuel@users.noreply.github.com>
2022-04-23 20:24:20 +0100

    NEW

---
+++ assets/js/index.js
@@ -0,0 +1,33 @@
+
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+                clifford: '#da373d',
+                 brand: {
+                    DEFAULT: "#ffff",
+                    primary: "#f97316",
+                    secondary: "#004a3a",
+                },
+          }
+        }
+        },
+         varients: {
+        extend: {
+            display: ["group-focus"],
+            opacity: ["group-focus"],
+            inset: ["group-focus"],
+        },
+    },
+    }
+
+    const mobileBtn = document.getElementById("btn");
+    const mobileMenu = document.getElementById("menu");
+    const svg = document.getElementById("Capa_1");
+    // const animate = document.getElementById("_1");
+    // const animate2 = document.getElementById("_2");
+    // const joy = `rotate-45`
+
+    mobileBtn.addEventListener("click", () => {
+        mobileMenu.classList.toggle("hidden");
+    });
