001947c0b09d1859a20f8eafd1b762d2e51cc582
Bieefilled <65086865+PhetSirmhuel@users.noreply.github.com>
2022-05-12 19:27:51 +0100

    maintainance checks

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,12 +1,21 @@
-const loginform = document.getElementById("login");
+const loginForm = document.getElementById("login");
 const otpverify = document.getElementById("verifyOtp");
 const phoneNum = document.getElementById("phoneNumber");
 const Submitbtn = document.getElementById("sign-in-button");
 const comfirmOtp = document.getElementById("loginsubmmit");
 const innerInfo = document.getElementById("container");
 const svg1 = document.getElementById("svg");
-
 const form = document.getElementById("formID");
+const formContent = document.querySelector(".formContent");
+
+const verifiedMenu = document.querySelector("#statMenu");
+verifiedMenu.addEventListener("click", verifyMenu);
+const verifiedContent = document.querySelector(".verifiedContent");
+
+function verifyMenu() {
+  formContent.classList.add("hidden");
+  verifiedContent.classList.remove("hidden");
+}
 
 //mobile login form
 const navlogin = document.querySelector("#mlogin");
@@ -18,13 +27,13 @@ navlogin.addEventListener("click", () => {
 const registerform = document.querySelector("#register");
 registerform.addEventListener("click", () => {
   registerform.classList.remove("hidden");
-  loginform.classList.add("hidden");
+  loginForm.classList.add("hidden");
 });
 
 const registerBtn = document.querySelector("#printIcon");
 registerBtn.addEventListener("click", () => {
   registerform.classList.remove("hidden");
-  loginform.classList.add("hidden");
+  loginForm.classList.add("hidden");
 });
 
 const loginbtn = document.querySelector("#loginbtn");
@@ -38,126 +47,24 @@ function Output(text) {
   output.innerHTML = newLine;
 }
 
-//EVERYTHING FIREBASE Auth
-// -STEP1 SETTING RECAPTCHA
-window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
-  "sign-in-button",
-  {
-    size: "invisible",
-    callback: (response) => {
-      // reCAPTCHA solved, allow signInWithPhoneNumber.
-      console.log("onload");
-      onSignInSubmit();
-    },
-    defaultCountry: "NG",
-  }
-);
-
-// DISPLAYING THE RECAPTURE WIDGET
-recaptchaVerifier.render().then((widgetId) => {
-  window.recaptchaWidgetId = widgetId;
-});
-
-function isvalid() {
-  const isValid = Validity.includes(phoneNum.value);
-  console.log(isValid);
-  return;
-}
-
-//functionn to verify if num exists
-function verifynum() {
-  // init fb
-  // get the doc - users
-  // find the object with phoneNum
-  const valid = data.includes(phoneNum.value);
-  console.log(valid);
-  return valid;
-}
-
-function onSignInSubmit(event) {
-  event.preventDefault();
-  //verify phone number by calling api
-
-  //if num is verified proceed and get
-  if (verifynum() == true) {
-    const phoneNumber = "+234" + phoneNum.value;
-    const appVerifier = window.recaptchaVerifier;
-    console.log(phoneNumber);
-
-    firebase
-      .auth()
-      .signInWithPhoneNumber(phoneNumber, appVerifier)
-      .then((confirmationResult) => {
-        window.confirmationResult = confirmationResult;
-        // ...
-        console.log("otp sent");
-        Output("[" + new Date().toLocaleTimeString() + "]: " + "OTP SENT");
-        document.querySelector(".icon").classList.remove("hidden");
-
-        return confirmationResult;
-      })
-      .catch((error) => {
-        const message = error.message;
-        Output(`${message}`);
-        document.querySelector(".icon").classList.remove("hidden");
-      });
-  } else {
-    //invalid form filled
-    Output(`NOT IN DATABASE`);
-    document.querySelector(".icon").classList.remove("hidden");
-  }
-}
-
-function onverifySubmit() {
-  const code = otpverify.value;
-  confirmationResult
-    .confirm(code)
-    .then((result) => {
-      const user = result.user;
-      // ...
-      console.log("successful");
-      console.log(user);
-      document.querySelector("#modalauth").classList.remove("hidden");
-    })
-    .catch((error) => {
-      const message = error.message;
-
-      Output(`${message}`);
-
-      console.log(message);
-    });
-}
-
-// signout
-function signoutbtn() {
-  firebase
-    .auth()
-    .signOut()
-    .then(() => {
-      window.location.assign("/");
-    })
-    .catch((error) => {
-      // An error happened.
-    });
-}
-// Passwordless integration
-// const apiKey = "demobackend:public:c203e65b581443778ea4823b3ef0d6af";
-// const backendUrl = "https://demo-backend.passwordless.dev";
-
 async function Signin(alias) {
+  var challenge = new Uint8Array(32);
+  window.crypto.getRandomValues(challenge);
+
   var userID = "Kosv9fPtkDoh4Oz7Yq/pVgWHS8HhdlCto5cR0aBoVMw=";
   var id = Uint8Array.from(window.atob(userID), (c) => c.charCodeAt(0));
+
   var publicKey = {
     challenge: challenge,
 
     rp: {
-      name: "yct security system",
+      name: "Example Inc.",
     },
 
     user: {
       id: id,
-      name: alias,
-      displayName: "bieefilled",
+      name: "alice@example.com",
+      displayName: "Alice Liddell",
     },
 
     pubKeyCredParams: [
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,36 +1,29 @@
-
-    tailwind.config = {
-      theme: {
-        extend: {
-          colors: {
-                clifford: '#da373d',
-                 brand: {
-                    DEFAULT: "#ffff",
-                    primary: "#f97316",
-                    secondary: "#004a3a",
-                },
-          }
-        }
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        brand: {
+          DEFAULT: "#ffff",
+          primary: "#f97316",
+          secondary: "#004a3a",
         },
-         varients: {
-        extend: {
-            display: ["group-focus"],
-            opacity: ["group-focus"],
-            inset: ["group-focus"],
-        },
+      },
     },
-    }
+  },
+  varients: {
+    extend: {
+      display: ["group-focus"],
+      opacity: ["group-focus"],
+      inset: ["group-focus"],
+    },
+  },
+};
 
-    const mobileBtn = document.getElementById("btn");
-    const mobileMenu = document.getElementById("menu");
-    const svg = document.getElementById("Capa_1");
-    // const animate = document.getElementById("_1");
-    // const animate2 = document.getElementById("_2");
-    // const joy = `rotate-45`
+const mobileBtn = document.getElementById("btn");
+const mobileMenu = document.getElementById("menu");
+const svg = document.getElementById("Capa_1");
 
-    mobileBtn.addEventListener("click", () => {
-        mobileMenu.classList.toggle("hidden");
-    });
-
-    var challenge = new Uint8Array(32);
-window.crypto.getRandomValues(challenge);
+mobileBtn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
